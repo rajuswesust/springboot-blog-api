a36f985097852e959460573d8af13ab4053d21da
rajuswesust <singhoraju8@gmail.com>
2023-10-09 00:07:30 +0600

    allowed all origin: CORS

--- src/main/java/com/spring/boot/blog/app/RestApiApplication.java
+++ src/main/java/com/spring/boot/blog/app/RestApiApplication.java
@@ -8,6 +8,8 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import org.modelmapper.*;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class RestApiApplication {
@@ -19,6 +21,17 @@ public class RestApiApplication {
 	}
 	public static void main(String[] args) {
 		SpringApplication.run(RestApiApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").
+						allowedOrigins("*");
+			}
+		};
 	}
 
 //	public static void main(String[] args) {
--- src/main/java/com/spring/boot/blog/app/config/SecurityConfiguration.java
+++ src/main/java/com/spring/boot/blog/app/config/SecurityConfiguration.java
@@ -25,7 +25,7 @@ public class SecurityConfiguration {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeHttpRequests()
-                .requestMatchers("/api/auth/**")
+                .requestMatchers("/api/auth/**", "/api/public")
                 .permitAll()
                 .anyRequest()
                 .authenticated()
